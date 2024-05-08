eabee08e9c059f2a8b319601cb293d99a1a4fb37
AmandaHockmuth <153574152+AmandaHockmuth@users.noreply.github.com>
2024-05-07 23:25:19 -0400

    Created Results section and made it hidden until returned, completed function to allow for user input to complete the fetch url and then append the results list to results section upon its return, updated TO DO lists

--- js/index.js
+++ js/index.js
@@ -1,19 +1,49 @@
-fetch(
-  `https://ecos.fws.gov/ecp/pullreports/catalog/species/report/species/export?format=json&distinct=true&columns=%2Fspecies%40cn%2Cstatus%2Cmore_info_url&sort=%2Fspecies%40cn%20asc&filter=%2Fspecies%2Fcurrent_range_county%40name%20%3D%20'Rockbridge'`
-)
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error(`${response.status}`);
-    }
-    console.log(response.json());
-  })
-  .catch((err) => {
-    console.error(err);
-  });
+const inputForm = document.querySelector("form");
+const resultsSection = document.querySelector("#results");
+const resultsList = document.querySelector("ul");
+
+inputForm?.addEventListener("submit", (submission) => {
+  submission.preventDefault();
+  resultsSection.style.display = "block";
+  const userInput = submission.target?.userInput.value;
+  // website used to generate template https://ecos.fws.gov/ecp/report/adhocCreator?catalogId=species&reportId=species&columns=%2Fspecies@cn,status&sort=%2Fspecies@cn%20asc&distinct=true&filter=%2Fspecies%2Fcurrent_range_county@name%20%3D%20%27Wake%27
+  // changed fetch url template below to temporarily return only common name and status incase the null more_info_url data points was throwing it off
+  const fetchURL = `https://ecos.fws.gov/ecp/pullreports/catalog/species/report/species/export?format=json&distinct=true&columns=%2Fspecies%40cn%2Cstatus%2Cmore_info_url&sort=%2Fspecies%40cn%20asc&filter=%2Fspecies%2Fcurrent_range_county%40name%20%3D%20'${userInput}'`;
+  fetch(`${fetchURL}`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`${response.status}`);
+      }
+      return response.json();
+    })
+    .then((responseJSON) => {
+      const responseArray = responseJSON.data;
+      console.log(responseArray);
+      return responseArray;
+    })
+    .then((array) => {
+      for (i = 0; i < array.length; i++) {
+        const speciesData = array[i].toString();
+        const species = document.createElement("li");
+        // simplifying code to debug
+        // const speciesCommonName = results[i].cn;
+        // const speciesStatus = results[i].status;
+        // const speciesURL = results[i].more_info_url;
+        // species.innerHTML = `${speciesCommonName} is ${speciesStatus}. More information can be found on them here: ${speciesURL} (if available).`;
+        species.innerHTML = `${speciesData}`;
+        resultsList?.appendChild(species);
+      }
+    })
+    .catch((err) => {
+      const species = document.createElement("li");
+      species.innerHTML = `${err}`;
+      resultsList?.appendChild(species);
+    });
+  inputForm?.reset();
+});
 
 /*
 TODO
-Create a function to generate the url to query by taking the input county name from the user input and inserting it in the url
-Edit existing query to accept the generated url, and unhide Results section
-Edit then to return data to the page
+Fix second "then" to return data to the results section [Since some of the species do not have a url, the data point is null, could that be throwing off my for loop?]
+Deal with errors/instructions for submitting county names (possibly add state abbreviation input)
 */
